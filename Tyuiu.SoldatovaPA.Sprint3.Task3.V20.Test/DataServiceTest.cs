6b2133254d8b6310c27e4cc7041819641e7d7930
PolinaSold <clevnay@mail.ru>
2025-11-23 16:56:39 +0500

    Task3

---
+++ Tyuiu.SoldatovaPA.Sprint3.Task3.V20.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.SoldatovaPA.Sprint3.Task3.V20.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint3.Task3.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCharCount()
+        {
+            DataService ds = new DataService();
+            string str = "gfft ntf f opf";
+            char ch = 'f';
+            int res = ds.GetCharCount(str, ch);
+            int wait = 5; // Ожидаемое количество: gfft(2) + ntf(1) + f(1) + opf(1) = 5
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SoldatovaPA.Sprint3.Task3.V20/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Tyuiu.SoldatovaPA.Sprint3.Task3.V20.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint3.Task3.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнила: Солдатова П. А. | ИСПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                           *");
+            Console.WriteLine("* Задание #3                                                             *");
+            Console.WriteLine("* Вариант #20                                                            *");
+            Console.WriteLine("* Выполнила: Солдатова П. А. | ИСПБ-25-1                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Используя цикл foreach подсчитать количество букв 'f' в строке:       *");
+            Console.WriteLine("* 'gfft ntf f opf'                                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            string str = "gfft ntf f opf";
+            char ch = 'f';
+
+            Console.WriteLine($"Исходная строка = {str}");
+            Console.WriteLine($"Искомый символ = {ch}");
+
+            DataService ds = new DataService();
+            int result = ds.GetCharCount(str, ch);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Количество символов '{ch}' в строке = {result}");
+            Console.ReadKey();
+        }
+    }
+}
