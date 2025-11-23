aa005ea10cf65cc9b9f12a3ed757cd4a9da84130
PolinaSold <clevnay@mail.ru>
2025-11-23 17:37:40 +0500

    Task7

---
+++ Tyuiu.SoldatovaPA.Sprint3.Task7.V6.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+using Tyuiu.SoldatovaPA.Sprint3.Task7.V6.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint3.Task7.V6.Lib
+{
+    public class DataService : ISprint3Task7V6
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int length = stopValue - startValue + 1;
+            double[] valueArray = new double[length];
+
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double denominator = 4 * x - 0.5;
+
+                // Проверка деления на ноль (когда 4x - 0.5 = 0)
+                if (Math.Abs(denominator) < 0.0001)
+                {
+                    valueArray[count] = 0;
+                }
+                else
+                {
+                    double term1 = (3 * Math.Cos(x)) / denominator;
+                    double term2 = Math.Sin(x) - 5 * x - 2;
+                    valueArray[count] = Math.Round(term1 + term2, 2);
+                }
+                count++;
+            }
+
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.SoldatovaPA.Sprint3.Task7.V6.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.SoldatovaPA.Sprint3.Task7.V6.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint3.Task7.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double[] res = ds.GetMassFunction(startValue, stopValue);
+            double[] wait = {
+                15.19, 11.67, 8.65, 5.53, 2.26, 0, -3.30, -6.44, -9.63, -12.78, -15.91
+            };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
